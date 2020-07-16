77308500cd66dc86ff10b292f01e5b09fa68954b
najmulalammirajbd <najmulalammiraj@gmail.com>
2020-07-17 02:16:06 +0600

    my work done

---
+++ assignment.js
@@ -0,0 +1,32 @@
+//Feet To Mile
+function feetToMile (feet){
+    var mile = feet / 5280;
+    return mile;
+
+}
+var nanaMile = feetToMile(15840);
+console.log(nanamile);
+
+//Woodcal Culator
+var chair = 14*1;
+var table = 5*3;
+var bed = 12*5;
+var woodcalculator = ( chair + table + bed );
+console.log(woodcalculator);
+
+//brick Calulator
+var oneToTen = 15*1000;
+var oneToTenResult = oneToTen*10;
+var elevenToTwenty = 12*1000;
+var elevenToTwentyResult = elevenToTwenty*10;
+var twentyOneToTwentySeven = 10*1000;
+var twentyOneToTwentySevenResult = twentyOneToTwentySeven*7;
+var brickCalculator = oneToTenResult + elevenToTwentyResult + twentyOneToTwentySevenResult;
+console.log(brickCalculator);
+
+//tinyFriend
+var tinyFriend = [ "miraj" , "fahim" , "afwan" , "mahdhi" , "marjan" , "rezbi"]
+var searchNeme = tinyFriend.indexOf(9);
+console.log(searchNeme);
+
+
